3acdcabe6b185be533e4e6b7708e684c508d4a49
Hannes Erikson <eriksonhannes@gmail.com>
2019-08-06 10:43:51 +0200

    improved parsing and fix #3 more options

--- doctool.py
+++ doctool.py
@@ -6,26 +6,38 @@ print("Welcome, this tool will help you to quickly generate a short summary of y
 fileName = input("Please enter the search path of your file (including file ending): ")
 f = open(fileName, "r")
 commentChars = input("Please enter the comment character(s) of your document: ")
-
+ccl = len(commentChars)                             # The number of comment characters
+ccl2 = ccl + 2
+ccl3 = ccl + 3
 line = 1
 numFunc = 0
+
 print("--- Functions ---")
 for x in f:                                         # Check every line in the document
     x = x.strip()                                   # Removes whitespaces
     if len(x) > 1:
-        if (x[:len(commentChars)] == commentChars): # Checks for comments using the specified comment character(s)
-            if ('f:' in x):                         # Function description
-                print("Line " + str(line) + ":")
-                print(x[2:])
+        if (x[:ccl] == commentChars):               # Checks for comments using the specified comment character(s)
+            if ('f:' in x[:(ccl3)]):                # Function description
+                print("Line " + str(line) + ":" + x[(ccl3):])
                 numFunc = numFunc+1
-                
-            elif ('p:' in x):                       # Params
-                print(x[2:])
-            elif ('c:' in x):                       # Complexity
-                print(x[2:])
+            elif ('d:' in x[:(ccl3)]):             # Description of the function
+                print("Despcription:" + x[(ccl3):])
+            elif ('p:' in x[:(ccl3)]):             # Params
+                print("Parameters: " + x[(ccl3):])
+            elif ('c:' in x[:(ccl3)]):             # Complexity
+                print("Complexity: " + x[(ccl3):])
+            elif ('o:' in x[:(ccl3)]):             # Output
+                print("Complexity: " + x[(ccl3):])
+            
             
     line = line + 1
 
 print("--- Summary ---")                            # Print summary
 print("Number of lines: " + str(line))
 print("Number of documented functions: " + str(numFunc))
+
+# For testing
+# f: nothingFunc
+# d: Function that does nothing
+# p: None
+#c: O(1)
